092506b33c1417ecd95cf6aab5a80207c6446b45
zulqarnain-99 <zulqi99@gmail.com>
2020-12-20 13:16:09 +0500

    multiview completed

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -11,7 +11,42 @@ namespace Multiview_Control_ASP.NET
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                multiViewEmployee.ActiveViewIndex = 0;
+            }
+        }
 
+        protected void btnGoToStep2_Click(object sender, EventArgs e)
+        {
+            multiViewEmployee.ActiveViewIndex = 1;
+        }
+
+        protected void btnBackToStep1_Click(object sender, EventArgs e)
+        {
+            multiViewEmployee.ActiveViewIndex = 0;
+        }
+
+        protected void btnGoToStep3_Click(object sender, EventArgs e)
+        {
+            lblFirstName.Text = txtFirstName.Text;
+            lblLastName.Text = txtLastName.Text;
+            lblGender.Text = ddlGender.SelectedValue;
+
+            lblEmail.Text = txtEmail.Text;
+            lblMobile.Text = txtMobile.Text;
+
+            multiViewEmployee.ActiveViewIndex = 2;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            multiViewEmployee.ActiveViewIndex = 1;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Confirmation.aspx");
         }
     }
 }
